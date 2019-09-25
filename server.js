e20f31cc0eeb547a5d15c8ca60de883ba44f494c
Marcohiro <petit.marcalain@yahoo.fr>
2019-09-25 12:04:49 +0200

    Ajout get params

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function (req, res) {
     res.send('Quel est votre nom ?')
-    res.send(req.params.name)
+    if(req.params.nom == 'Sasha' || req.params.nom == 'Sasha') res.send ('Bonjour ' + req.params.nom)
+    else{
+        res.send('Quel est votre prenom?')
+    }
   })
 
 app.listen(process.env.PORT, function () {
