4a88b360efac9a5fb2b31a5e88b1db2adc58e49b
Marcohiro <petit.marcalain@yahoo.fr>
2019-09-25 12:58:57 +0200

    Ajout post chat

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -12,6 +14,18 @@ app.get('/hello', function (req, res) {
         res.send('Quel est votre prenom?')
     }
   })
+
+app.post('/chat', function(req, res){
+    if(req.body != null) {
+        if (req.body.msg == "ville") {
+            res.send("Nous sommes à Paris")
+        } else if(req.body.msg == "météo"){
+            res.send("Il fait beau")
+        }
+    }else{
+        res.send('Inserer message')
+    }
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port')
